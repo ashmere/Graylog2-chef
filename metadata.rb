35b18f759926113278bb4541b7d8a4971ce021ba
Mat Davies <ashmere@gmail.com>
2013-07-25 16:29:05 +0100

    minor change enabling support for debian and version change

--- metadata.rb
+++ metadata.rb
@@ -3,11 +3,12 @@ maintainer        "Phil Sturgeon"
 maintainer_email  "email@philsturgeon.co.uk"
 license           "Apache 2.0"
 description       "Installs and configures Graylog2"
-version           "0.1.0"
+version           "0.1.1"
 recipe            "graylog2", "Installs and configures Graylog2"
 
-# Only supporting Ubuntu 10.x and up
+# Only supporting Debian, Ubuntu 10.x and up
 supports "ubuntu"
+supports "debian"
 
 # OpsCode cookbook dependencies
 depends "apt"     # http://community.opscode.com/cookbooks/apt
